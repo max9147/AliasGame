d28a7c0c3c8d19ab2220e5009a3f2098cf31e5af
max9147 <m.chizh99@gmail.com>
2021-11-15 20:43:11 +0300

    Themes object

---
+++ Assets/Scripts/ScriptableObjects/Themes.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu]
+public class Themes : ScriptableObject
+{
+    public string themeName;
+    public Image themeImage;
+    public string[] words;
+}
