2ac7ce9d7fa922ede149a5fbc638e3116fbfd0d9
max9147 <m.chizh99@gmail.com>
2021-11-15 21:54:13 +0300

    Gameplay Scene

--- Assets/Scripts/MenuNavigation.cs
+++ Assets/Scripts/MenuNavigation.cs
@@ -5,4 +5,19 @@ using UnityEngine;
 public class MenuNavigation : MonoBehaviour
 {
     public GameObject[] menus;
+
+    private int curMenu = 0;
+
+    public void OpenMenu(int menuID)
+    {
+        menus[0].SetActive(false);
+        menus[menuID].SetActive(true);
+        curMenu = menuID;
+    }
+
+    public void ReturnToMain()
+    {
+        menus[curMenu].SetActive(false);
+        menus[0].SetActive(true);
+    }
 }
