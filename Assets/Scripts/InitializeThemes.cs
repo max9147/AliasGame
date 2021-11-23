235b436ba7a10d0b5d2da42af84cecb2302cd7af
max9147 <m.chizh99@gmail.com>
2021-11-23 19:47:16 +0300

    Team configuration

--- Assets/Scripts/InitializeThemes.cs
+++ Assets/Scripts/InitializeThemes.cs
@@ -6,9 +6,12 @@ using UnityEngine;
 public class InitializeThemes : MonoBehaviour
 {
     public GameObject themeButtonPrefab;
-    public GameObject mainContainer;
-    public GameObject freeContainer;
-    public GameObject paidContainer;
+    public GameObject mainContainerDuel;
+    public GameObject freeContainerDuel;
+    public GameObject paidContainerDuel;
+    public GameObject mainContainerTeam;
+    public GameObject freeContainerTeam;
+    public GameObject paidContainerTeam;
     public Themes[] freeThemes;
     public Themes[] paidThemes;
 
@@ -18,21 +21,35 @@ public class InitializeThemes : MonoBehaviour
     {
         foreach (var item in freeThemes)
         {
-            curButton = Instantiate(themeButtonPrefab, freeContainer.transform);
+            curButton = Instantiate(themeButtonPrefab, freeContainerDuel.transform);
+            curButton.transform.Find("ThemeName").GetComponent<TextMeshProUGUI>().text = item.themeName;
+            curButton.GetComponent<ThemeSelection>().theme = item;
+            curButton.GetComponent<ThemeSelection>().menuSystem = gameObject;
+
+            curButton = Instantiate(themeButtonPrefab, freeContainerTeam.transform);
             curButton.transform.Find("ThemeName").GetComponent<TextMeshProUGUI>().text = item.themeName;
             curButton.GetComponent<ThemeSelection>().theme = item;
             curButton.GetComponent<ThemeSelection>().menuSystem = gameObject;
         }
-        freeContainer.GetComponent<RectTransform>().sizeDelta = new Vector3(100, Mathf.Ceil(freeThemes.Length / 4f) * 245);
+        freeContainerDuel.GetComponent<RectTransform>().sizeDelta = new Vector3(100, Mathf.Ceil(freeThemes.Length / 4f) * 245);
+        freeContainerTeam.GetComponent<RectTransform>().sizeDelta = new Vector3(100, Mathf.Ceil(freeThemes.Length / 4f) * 245);
+
         foreach (var item in paidThemes)
         {
-            curButton = Instantiate(themeButtonPrefab, paidContainer.transform);
+            curButton = Instantiate(themeButtonPrefab, paidContainerDuel.transform);
+            curButton.transform.Find("ThemeName").GetComponent<TextMeshProUGUI>().text = item.themeName;
+            curButton.GetComponent<ThemeSelection>().theme = item;
+            curButton.GetComponent<ThemeSelection>().menuSystem = gameObject;
+
+            curButton = Instantiate(themeButtonPrefab, paidContainerTeam.transform);
             curButton.transform.Find("ThemeName").GetComponent<TextMeshProUGUI>().text = item.themeName;
             curButton.GetComponent<ThemeSelection>().theme = item;
             curButton.GetComponent<ThemeSelection>().menuSystem = gameObject;
         }
-        paidContainer.GetComponent<RectTransform>().sizeDelta = new Vector3(100, Mathf.Ceil(paidThemes.Length / 4f) * 245);
+        paidContainerDuel.GetComponent<RectTransform>().sizeDelta = new Vector3(100, Mathf.Ceil(paidThemes.Length / 4f) * 245);
+        paidContainerTeam.GetComponent<RectTransform>().sizeDelta = new Vector3(100, Mathf.Ceil(paidThemes.Length / 4f) * 245);
 
-        mainContainer.GetComponent<RectTransform>().sizeDelta = new Vector3(1000, 150 + freeContainer.GetComponent<RectTransform>().sizeDelta.y + paidContainer.GetComponent<RectTransform>().sizeDelta.y);
+        mainContainerDuel.GetComponent<RectTransform>().sizeDelta = new Vector3(1000, 150 + freeContainerDuel.GetComponent<RectTransform>().sizeDelta.y + paidContainerDuel.GetComponent<RectTransform>().sizeDelta.y);
+        mainContainerTeam.GetComponent<RectTransform>().sizeDelta = new Vector3(1000, 150 + freeContainerDuel.GetComponent<RectTransform>().sizeDelta.y + paidContainerDuel.GetComponent<RectTransform>().sizeDelta.y);
     }
 }
